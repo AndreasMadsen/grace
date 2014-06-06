4ffa2bc56a9709773fd5f2be2fcf86645dc01087
frederikWR <frederik.ubuntu.wolgast@gmail.com>
2014-06-06 09:36:09 +0200

    Changed transparency, added savefig and figzie=(12,6)

--- Code/visualize_curves.py
+++ Code/visualize_curves.py
@@ -16,13 +16,13 @@ shape = grace.load.grids.shape
 Y = np.asmatrix(grace.load.grids.reshape(shape[0] * shape[1], shape[2]))
 Y=Y.A
 
-fig = plt.figure()
+fig = plt.figure(figsize=(12,6))
 
 # Plot y 
-r=Y.shape[0]
+r=Y.shape[0]-50000
 for i in range(r):
 	print 'Processing location ' + str(i+1) + ' out of  ' + str(r)
-	plt.plot(days, Y[i,:], 'b-',label='Observations',alpha=0.1)
+	plt.plot(days, Y[i,:], 'b-',label='Observations',alpha=0.05)
 
 
 date_ticks = np.linspace(np.min(days), np.max(days), 6).astype('int')
@@ -32,5 +32,6 @@ plt.xlim(np.min(days), np.max(days))
 plt.ylabel('EWH [m]')
 plt.xlabel('date')
 
-
+print 'saving and plotting figure'
+fig.savefig("../Rapport/figures/transparent_lines.png")
 plt.show()
