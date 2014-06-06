405612e9c263751608e68a8896bb3541b6c3c582
frederikWR <frederik.ubuntu.wolgast@gmail.com>
2014-06-06 09:28:33 +0200

    Added files to plot all locations ewh

---
+++ Code/visualize_curves.py
@@ -0,0 +1,36 @@
+
+import grace
+import grace.times
+import grace.ols
+import numpy as np
+import matplotlib.pyplot as plt
+
+initial = (24, 134)
+
+# get x values (in days)
+days = grace.ols.time_vector()
+all_days = np.linspace(np.min(days), np.max(days), np.max(days) - np.min(days))
+
+# Get the ewh values on columns, and positions on rows
+shape = grace.load.grids.shape
+Y = np.asmatrix(grace.load.grids.reshape(shape[0] * shape[1], shape[2]))
+Y=Y.A
+
+fig = plt.figure()
+
+# Plot y 
+r=Y.shape[0]
+for i in range(r):
+	print 'Processing location ' + str(i+1) + ' out of  ' + str(r)
+	plt.plot(days, Y[i,:], 'b-',label='Observations',alpha=0.1)
+
+
+date_ticks = np.linspace(np.min(days), np.max(days), 6).astype('int')
+plt.xticks(date_ticks, grace.times.days_to_str(date_ticks))
+plt.xlim(np.min(days), np.max(days))
+
+plt.ylabel('EWH [m]')
+plt.xlabel('date')
+
+
+plt.show()
