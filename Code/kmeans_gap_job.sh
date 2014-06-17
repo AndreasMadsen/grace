1376abad3a966d656a21ab5bb66bfb6af0527a36
Andreas Madsen <amwebdk@gmai.com>
2014-06-17 21:26:16 +0200

    [hpc] output from kmeans-gap dumped

--- Code/kmeans_gap_job.sh
+++ Code/kmeans_gap_job.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 #PBS -N kmeans-gap
-#PBS -l walltime=01:00:00
-#PBS -l nodes=1:ppn=8
+#PBS -l walltime=04:00:00
+#PBS -l nodes=1:ppn=2
 #PBS -m eba
 #PBS -M amwebdk@gmail.com
 
 cd $PBS_O_WORKDIR
 
-python kmeans_gap_job.py 8
+python kmeans_gap_job.py 2
