964cbe2d14500a79e3cb627510761878c3cc727e
Andreas Madsen <amwebdk@gmail.com>
2014-06-23 14:03:26 +0200

    [script] estimate ice melting

--- Code/ice_estimate.py
+++ Code/ice_estimate.py
@@ -5,14 +5,18 @@ import grace.ols
 import grace.gia
 import grace.mask
 import numpy as np
+import matplotlib.pyplot as plt
+import scipy.integrate
 
 (lons, lats) = (grace.positions[:, :, 1], grace.positions[:, :, 0])
-EARTH_RADIUS = 6371.0 # km
+EARTH_RADIUS = 6371000.0 # m
 
 #
 # Calculate GIA velocity
 #
-velocity = grace.ols.theta_matrix()[:, :, 1]
+theta = grace.ols.theta_matrix()
+velocity = theta[:, :, 1]
+acceleration = theta[:, :, 2]
 data = (velocity - grace.gia.grid)
 
 #
@@ -33,6 +37,19 @@ area_map = area(lons, lats)
 # Mask velocity and area
 #
 mask = grace.mask.greenland().reshape((180 * 360))
-greenland_area = area_map.reshape((180 * 360))[mask]
-greanland_vel = velocity.reshape((180 * 360))[mask]
+greenland_area = area_map.reshape((180 * 360))[mask] # m^2
+greanland_vel = velocity.reshape((180 * 360))[mask] # m
+greanland_acc = acceleration.reshape((180 * 360))[mask] # m
+
+ewh = lambda t: np.sum(greenland_area * (greanland_vel * t + 0.5 * greanland_acc * t**2) * 997.0) # kg
+
+days = grace.ols.time_vector()
+years = np.unique(grace.times.days_to_date(days).astype('datetime64[Y]'))
+years_str = years.astype('str')
+years_days = grace.times.date_to_days(years.astype('datetime64[D]'))
+
+for i in range(0, years.shape[0] - 1):
+	loss = -1 * scipy.integrate.quad(lambda t: ewh(t) - ewh(years_days[i]), years_days[i], years_days[i + 1])[0]
+
+	print "From %s to %s, mass loss was %f giga ton" % (years_str[i], years_str[i + 1], loss / (10**12))
 
